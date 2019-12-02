37406be8502b1bc08fff59c3ae7baacdae61a819
Rafael Teyral <rafael.teyral@hyke.at>
2019-12-02 22:27:27 +0100

    minor cleanup

--- Program.fs
+++ Program.fs
@@ -8,8 +8,8 @@ let main argv =
     printfn "Hello World I'm ready for advent of code"
     let readLine = Console.ReadLine();
     let result = match readLine with
-        | "1" -> DayOne.dayOne()
-        | "1.2" -> DayOne.partTwo()
-        | _ -> "the day has not come yet"
+                    | "1" -> DayOne.partOne()
+                    | "1.2" -> DayOne.partTwo()
+                    | _ -> "the day has not come yet"
     printfn "%s" result
     0
--- days/DayOne.fs
+++ days/DayOne.fs
@@ -1,8 +1,8 @@
 module DayOne
 
-let basePath = "C:\Dev\AdventOfCode\AdventOfCode19\days";
-
 let calculateFuel mass = mass / 3 - 2
+
+let inputLines = System.IO.File.ReadLines "input/day1.txt"
 
 let rec calculateFuelRec mass =
     let fuel = calculateFuel mass
@@ -11,22 +11,20 @@ let rec calculateFuelRec mass =
     else
         fuel + calculateFuelRec fuel
 
-let dayOne() =
+let partOne() =
     printfn "day 1"
-    let inputLines = System.IO.File.ReadLines "days/day1.txt"
-    let result = inputLines
-                |> List.ofSeq
-                |> List.map int
-                |> List.map calculateFuel
-                |> List.sum
-    string result
+    inputLines
+        |> List.ofSeq
+        |> List.map int
+        |> List.map calculateFuel
+        |> List.sum
+        |> string
     
 let partTwo() =
     printfn "day 1 part 2"
-    let inputLines = System.IO.File.ReadLines "days/day1.txt"
-    let result = inputLines
-                |> List.ofSeq
-                |> List.map int
-                |> List.map calculateFuelRec
-                |> List.sum
-    string result
+    inputLines
+        |> List.ofSeq
+        |> List.map int
+        |> List.map calculateFuelRec
+        |> List.sum
+        |> string
